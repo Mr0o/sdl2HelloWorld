4b25fc12c955062ac3401c50fbd410bdd183a244
Owen Smith <owen.smith1201@gmail.com>
2023-10-12 22:39:09 -0400

    Basic test window

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,19 @@
+// command to compile
+// Linux:   g++ -o bin/main src/main.cpp -I include -L lib -l SDL2
+
 #include <SDL2/SDL.h>
 #include <iostream>
 
+int main(int argc, char* argv[]){
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window* window = SDL_CreateWindow("Hello World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    SDL_Delay(3000);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
