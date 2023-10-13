bd928818656aac6b32641716f709e730246374e6
Owen Smith <owen.smith1201@gmail.com>
2023-10-13 17:11:43 -0400

    Set the window resolution to half the display resolution

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,29 @@
 #include <iostream>
 
 int main(int argc, char* argv[]){
+    // initialize SDL
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window* window = SDL_CreateWindow("Hello World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+
+    // get the resolution of the display
+    int x, y;
+    SDL_DisplayMode DM;
+    SDL_GetCurrentDisplayMode(0, &DM);
+    x = DM.w; y = DM.h;
+
+    // set the window resolution to half of the display resolution
+    x /= 2; y /= 2;
+
+    // create the window and renderer
+    SDL_Window* window = SDL_CreateWindow("Hello World! - Mr0o", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, x, y, SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    // set the color of the window to red, and wait 3 seconds
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
     SDL_Delay(3000);
+
+    // destroy the renderer and window, then quit
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
